22cd42136b7f5958a0f4082a0201ce44f7981aab
Joao-Pedro048 <soncinjoao@gmail.com>
2023-02-28 13:52:12 +0000

    Operadores

---
+++ Operadores.java
@@ -0,0 +1,13 @@
+public class Operadores{
+    public static void main(String[] barela){
+
+        int idade;
+        idade = 16;
+        idade = 17;
+        idade ++;
+        idade ++;
+        System.out.println("Eu tenho " + idade + " anos!");
+        idade+=10;
+        System.out.println(idade);
+    }
+}
