5fe62465cdca660419e7967224a86384085ae670
Joao-Pedro048 <soncinjoao@gmail.com>
2023-02-28 14:03:39 +0000

    ba

--- BalancoSimestral.java
+++ BalancoSimestral.java
@@ -1,6 +1,9 @@
 public class BalancoSimestral {
     public static void main(String[] args) {
-    int janeiro = 23000;
+    int janeiro = 15000;
+    int fevereiro = 23000;
+    int marco = 17000;
+    
 
 }
 }
